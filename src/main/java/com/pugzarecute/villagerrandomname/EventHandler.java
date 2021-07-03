f9d9e58baf6e0397f05b98f1d31868aef389aaff
PutoPug <me@pugzarecute.com>
2021-07-03 17:55:05 +0530

    Get some logic working

--- src/main/java/com/pugzarecute/villagerrandomname/EventHandler.java
+++ src/main/java/com/pugzarecute/villagerrandomname/EventHandler.java
@@ -1,13 +1,29 @@
 package com.pugzarecute.villagerrandomname;
 
+import com.electronwill.nightconfig.toml.TomlParser;
+import net.minecraft.entity.EntityType;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.loading.FMLPaths;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 @Mod.EventBusSubscriber(modid = VillagerRandomName.MODID,bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class EventHandler {
     @SubscribeEvent
     public static void onEntitySpawn(EntityJoinWorldEvent event){
-
+        if(event.getEntity().getType() == EntityType.VILLAGER){
+            if(!event.getEntity().hasCustomName()){
+                String[] nawme = {"Kritzkon","Raone","Fenris","Dolle","Synlek","Frank","Harold","Violager","Héloer",""};
+                ITextComponent name = ITextComponent.getTextComponentOrEmpty("UIEHFUIWE");
+                //TODO: COLORS //name.getStyle().setColor()
+                event.getEntity().setCustomName(name);
+            }
+        }
     }
 }
